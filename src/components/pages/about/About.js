ffe0e9305dca9165c5dbae25797efeacb562ff60
Hayaalsughair <hayaalsughair@gmail.com>
2024-05-20 10:09:56 +0000

    Build: BarCard/Fix: Banner-color/ Docs: Readme file/ add About => from Sham

--- src/components/pages/about/About.js
+++ src/components/pages/about/About.js
@@ -1,11 +1,39 @@
 import React from 'react'
-
-function About() {
+import './About.css'
+ function About() {
   return (
-    <div>
-      <>
-      </>
-    </div>
+  <>
+   <div className='about'>
+        <br/>
+        <h3>
+          we Are the Teams For JoyStick
+        </h3>
+
+        <br/>
+        <p>
+          Sham Omar
+        </p>
+        <br/>
+        <p>
+         Raneem khader
+        </p>
+        <br/>
+        <p>
+          Loay Al-Hasan
+        </p>
+        <br/>
+        <p>
+            Ahmad Abed
+        </p>
+        <br/>
+        <p>
+            Haya Sagher
+        </p>
+        <br/>
+        <a href="www.facebook.com"></a>
+      </div>
+
+  </>
   )
 }
 
--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Banner from '../../mainComponent/banner/Banner';
 import Cards from '../../mainComponent/cards/Cards';
+import BarCard from '../../mainComponent/barCard/BarCard';
 
 function Home() {
   return (
     <div>
     <Banner/>
+    <BarCard />
     <Cards/>
     </div>
   )
