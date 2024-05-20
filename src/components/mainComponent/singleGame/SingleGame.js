80c54a47425cc0acc394bc883a594c90b5ed0dae
Raneem-Hamid <raneemhamid511@gmail.com>
2024-05-20 12:58:46 +0300

    Build: favorite list,favorite card

--- src/components/mainComponent/singleGame/SingleGame.js
+++ src/components/mainComponent/singleGame/SingleGame.js
@@ -4,25 +4,21 @@ import './SingleGame.css';
 import { HiHeart } from "react-icons/hi";
 const gamesData = require( '../../../data/games.json');
 
-
+//Handle favoraite button => add fav game to DB
 
 function SingleGame() {
 
-    // const { id } = useParams();
-    // console.log(id);
-    // const result = gamesData.find(item =>item.id === id);
-    // console.log(result);
+    
     const { id } = useParams();
-  const numericId = Number(id); // Convert the string id to a number
-  console.log(numericId);
+  const numericId = Number(id); 
   
   const result = gamesData.find(item => item.id === numericId);
-  console.log(result);
 
   return (
     <>
 
 <div className="fixed right-6 bottom-6">
+  {/*  fav btn */}
       <button
         type="button"
         className="my-button"
