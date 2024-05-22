bc08b29d65b309ec238ebbf2c028a3bc5dceb4bc
Hayaalsughair <hayaalsughair@gmail.com>
2024-05-22 23:26:43 +0000

    Fix: BarCard, Empty Component, Change Logo, and some issues/Docs: edit logo in readme file

--- src/components/mainComponent/card/Card.js
+++ src/components/mainComponent/card/Card.js
@@ -5,16 +5,11 @@ import { Link } from 'react-router-dom';
 function Card(props) {
   return (
     <>
-    {/* {console.log(props.item.id)} */}
-
-    
+ 
     <div>
       <Link to={`/singlegame/${props.item.id}`}>
        <div className="card 1">
           <div className="card_image"> <img src={props.item.image}/> </div>
-          {/* <div className="card_title title-white">
-            <p>{props.item.title}</p>
-          </div> */}
         </div>
 </Link>
         <h2 className='title-white' >{props.item.title}</h2>
